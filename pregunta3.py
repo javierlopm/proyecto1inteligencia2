ce8e1361b13d6cf1680818eb5a802a8db436b091
Javier Lopez <javierloplom@gmail.com>
2017-02-17 10:18:44 -0400

    datos ya se leen como float, problemas de dimensiones al entrenar

--- pregunta3.py
+++ pregunta3.py
@@ -14,18 +14,24 @@ file = open("AmesHousing.csv","r")
 data = list(reader(file, delimiter=',', quotechar='\"'))
 data = np.matrix(data)
 
+
 nominal_cols = [3 ,6 ,7 ,8 ,9, 10,11,12,13,14
                ,15,16,17,22,23,24,25,26,28,29
                ,30,31,32,33,34,36,40,41,42,43
                ,54,56,58,59,61,64,65,66,73,74
                ,75,79,80]
 for col in nominal_cols:
-    data[1:,col] = from_nominal(data[1:,col])
+    data[:,col] = from_nominal(data[:,col])
+
+# Separando header de datos y convirtiendo a float
+data   = data[1:]
+header = data[0]
+data[data==''] = "NaN"
+data = data.astype(np.float)
 
 np.random.shuffle(data)
 
 # Conjunto de entrenamiento y de prueba sin la columna de id's
-data = data[0:]
 N = data.shape[0]
 n = data.shape[0]*0.8
 
@@ -36,6 +42,8 @@ training_d = np.column_stack((np.ones((n,1)),data[:n,:]))
 
 iterations = 200
 theta      = np.ones((1,data.shape[1]))
+
+# import pdb;  pdb.set_trace()
 
 res = iteraciones(training_d[:,:-1].T
                  ,training_d[:,-1]
